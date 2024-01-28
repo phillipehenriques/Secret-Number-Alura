063b9276e912f0bce98bc1ca043d53a108abe7ad
Phillipe Csorgo Henriques <106546716+phillipehenriques@users.noreply.github.com>
2024-01-28 16:07:42 -0300

    Construindo o código

---
+++ app.js
@@ -0,0 +1,8 @@
+alert('Bem vindo ao jogo do número secreto!')
+
+let numeroSecreto = 34;
+
+let chute = prompt('Tente adivinhar o número secreto! Escolha um número entre 1 e 100')
+
+if (numeroSecreto == numeroSecreto) {alert('Você acertou! 34')
+}
