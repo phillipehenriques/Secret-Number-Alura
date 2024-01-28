d0eff092e64f32e2bfa2688589add2b4ef97c9fe
Phillipe Csorgo Henriques <106546716+phillipehenriques@users.noreply.github.com>
2024-01-28 19:33:09 -0300

    atualização IF e ELSE

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 alert('Bem vindo ao jogo do número secreto!')
 
 let numeroSecreto = 34;
-
+console.log(numeroSecreto)
 let chute = prompt('Tente adivinhar o número secreto! Escolha um número entre 1 e 100')
 
-if (numeroSecreto == numeroSecreto) {alert('Você acertou! 34')
-}
+if (chute == numeroSecreto) {
+    alert(`Você acertou! O número era ${numeroSecreto}`)
+} else {
+    alert('Você errou :/')
+} 
